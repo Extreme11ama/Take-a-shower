41af1004b2f86b16e542a62a5c6e0d21f8927a1c
Extreme11ama <wasifram@gmail.com>
2026-04-23 18:21:33 -0700

    another info update

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "*"],
+    allow_origins=["http://localhost:5173", "https://take-a-shower.vercel.app"],
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
